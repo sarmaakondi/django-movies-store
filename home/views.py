7ed25733485d9eca5b1be7f18af3568275bc2109
sarmaakondi <sarmaakondivnm@icloud.com>
2024-10-20 13:14:46 +1100

    Update about view to pass the template_data

--- home/views.py
+++ home/views.py
@@ -8,4 +8,6 @@ def index(request):
 
 
 def about(request):
-    return render(request, "home/about.html")
+    template_data = {}
+    template_data["title"] = "About"
+    return render(request, "home/about.html", {"template_data": template_data})
